f1aacbab99d917e981d81ccacea87a91fa4e0b0c
Rik <riccardo.coltrinari@studenti.unicam.it>
2019-11-21 10:53:34 +0100

    Inserite le varie properties. Collegato db in localhost. Create e suddivise le cartelle MCR. Mappata la home per prova.

--- backend/emporio/src/main/java/com/emporio/emporio/EmporioApplication.java
+++ backend/emporio/src/main/java/com/emporio/emporio/EmporioApplication.java
@@ -2,9 +2,17 @@ package com.emporio.emporio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class EmporioApplication {
+
+	@RequestMapping("/")
+	public String name() {
+		return "ciao";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmporioApplication.class, args);
