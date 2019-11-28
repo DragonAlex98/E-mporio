9f8663ade092bf3c6d2f40a1980f52d531243503
Ugo <ugo.spinucci@studenti.unicam.it>
2019-11-28 22:17:43 +0100

    sistemati errori su form

--- frontend/emporio/src/app/product/product.module.ts
+++ frontend/emporio/src/app/product/product.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ProductRoutingModule } from '@src/app/product/product-routing.module';
 import { InsertProductComponent } from '@src/app/product/insert-product/insert-product.component';
 import { InsertProductFormComponent } from '@src/app/product/insert-product-form/insert-product-form.component';
 import { ProductComponent } from '@src/app/product/product/product.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [InsertProductComponent, InsertProductFormComponent, ProductComponent],
+  declarations: [
+    InsertProductComponent,
+    InsertProductFormComponent,
+    ProductComponent],
   imports: [
     CommonModule,
-    ProductRoutingModule
+    HttpClientModule,
+    ProductRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductModule { }
