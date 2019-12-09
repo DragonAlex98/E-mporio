5aa8de6d39951b14394545150994c67759a2b6e2
Rik <riccardo.coltrinari@studenti.unicam.it>
2019-12-09 14:20:24 +0100

    tolto auth0

--- frontend/emporio/src/app/nav-bar/nav-bar.component.ts
+++ frontend/emporio/src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
 import { InsertProductFormComponent } from '../product/insert-product-form/insert-product-form.component';
 import { InsertProductComponent } from '../product/insert-product/insert-product.component';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
@@ -15,7 +14,7 @@ export class NavBarComponent implements OnInit {
 
   shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 
-  constructor(public auth: AuthService, public dialog: MatDialog) {  }
+  constructor(public dialog: MatDialog) {  }
 
   ngOnInit() {
   }
