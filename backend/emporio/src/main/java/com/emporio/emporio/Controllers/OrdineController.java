331af1c6fee8e5875009b7c532f1af38b30f617b
Rik <riccardo.coltrinari@studenti.unicam.it>
2019-12-12 14:42:41 +0100

    modificato ordine controller

--- backend/emporio/src/main/java/com/emporio/emporio/Controllers/OrdineController.java
+++ backend/emporio/src/main/java/com/emporio/emporio/Controllers/OrdineController.java
@@ -1,11 +1,17 @@
 package com.emporio.emporio.Controllers;
 
+import javax.validation.Valid;
+
+import com.emporio.emporio.Models.Ordine;
+import com.emporio.emporio.Models.Prodotto;
 import com.emporio.emporio.Repositories.OrdineRepository;
+import com.emporio.emporio.Utils.OrderDetails;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +25,16 @@ public class OrdineController {
     private OrdineRepository orderRepository;
 
 
+
     @CrossOrigin(origins = {"*"})
     @RequestMapping(value = "/orders", method = RequestMethod.POST)
-    public ResponseEntity<String> createNewOrder() {
+    public ResponseEntity<String> createNewOrder(@Valid @RequestBody Ordine newOrdine) {
+        ProdottoController controllerProd = ProdottoController.getInstance();
+
+        for(OrderDetails orderDetail : newOrdine.getOrderDetails()) {
+            orderDetail.setProdotto(controllerProd.getProductById(orderDetail.getProdotto().getProductId()).getBody().get());
+            orderDetail.setOrdine(newOrdine);
+        }
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
