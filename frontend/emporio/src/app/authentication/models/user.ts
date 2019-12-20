0ad8de142ed4904e30adc825a43cce5b1c3ea22e
Ugo <ugo.spinucci@studenti.unicam.it>
2019-12-20 11:52:02 +0100

    Aggiunto costruttore utente

--- frontend/emporio/src/app/authentication/models/user.ts
+++ frontend/emporio/src/app/authentication/models/user.ts
@@ -8,4 +8,11 @@ export class User {
     role: Role;
     token?: string;
 
+    constructor(id: number, username: string, password: string, role: Role){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
 }
