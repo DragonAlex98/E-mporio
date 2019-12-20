4c3611bf4829c5a49fe5987d31ab516114142a97
Ugo <ugo.spinucci@studenti.unicam.it>
2019-12-20 16:21:00 +0100

    Termine pagina login e registrazione

--- frontend/emporio/src/app/authentication/registration/registration.component.ts
+++ frontend/emporio/src/app/authentication/registration/registration.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
 
 import { UserService } from '@src/app/authentication/services/user.service';
 import {AuthenticationService} from '@src/app/authentication/services/authentication.service';
+import { Role } from '../models/role';
 
 @Component({templateUrl: 'registration.component.html'})
 export class RegistrationComponent implements OnInit {
     registerForm: FormGroup;
     submitted = false;
+    role = Role;
+    roles = Object.values(this.role); // array per la visualizzazione dell'enum dei ruoli
 
     constructor(
         private formBuilder: FormBuilder,
@@ -26,14 +28,15 @@ export class RegistrationComponent implements OnInit {
     ngOnInit() {
         this.registerForm = this.formBuilder.group({
             username: ['', Validators.required],
-            password: ['', [Validators.required, Validators.minLength(10)]]
+            password: ['', [Validators.required, Validators.minLength(10)]],
+            role : ['', [Validators.required]]
         });
     }
 
     onSubmit() {
         this.submitted = true;
 
-        // stop here if form is invalid
+        // se la form non e' valida non effettuo neanche la chiamata
         if (this.registerForm.invalid) {
             return;
         }
