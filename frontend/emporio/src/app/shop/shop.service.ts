3f2480000c2059c20105f189a139840e742c4ab4
DragonAlex98 <alessandro.antinori@studenti.unicam.it>
2020-01-06 21:29:21 +0100

    Aggiunti metodi eliminazione attività e catalogo

--- frontend/emporio/src/app/shop/shop.service.ts
+++ frontend/emporio/src/app/shop/shop.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Shop, ShopAdapter } from './shop/shop';
 import { environment } from '@src/environments/environment';
 import { map, tap } from 'rxjs/operators';
+import { AuthenticationService } from '../authentication/services/authentication.service';
+import { User } from '../authentication/models/user';
+import { Product, ProductCategory } from '../product/product/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
-  constructor(private httpClient: HttpClient, private adapter: ShopAdapter) { }
+  constructor(private httpClient: HttpClient, private adapter: ShopAdapter, private auth: AuthenticationService) { }
 
   searchShops (term: string): Observable<Shop[]> {
     return this.httpClient.get<Shop[]>(`${environment.apiUrl}/shops/search?ragSociale=${term}`).pipe(
@@ -27,4 +30,26 @@ export class ShopService {
                           .pipe();
   }
 
+  getShopFromPIVA(piva: string): Observable<Shop> {
+    return this.httpClient.get<Shop>(`${environment.apiUrl}/shops/${piva}`).pipe(
+      map((data: Shop) => new Shop(data.shopPIVA, data.shopAddress, data.shopBusinessName, 1, data.shopHeadquarter))
+    );
+  }
+
+  httpOptions = {
+  };
+
+  deleteShop(): Observable<string> {
+    this.httpOptions = {headers: new HttpHeaders({ 'Authorization': 'Bearer ' + this.auth.currentUserValue.token })};
+    console.log(this.httpOptions);
+    return this.httpClient.delete(`${environment.apiUrl}/shops`, this.httpOptions).pipe(
+      map((res: string) => res)
+    );
+  }
+
+  getShopCatalog(piva: string): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(`${environment.apiUrl}/shops/${piva}/products`).pipe(
+      map((data: any[]) => data.map(item => new Product(0, item.productName, new ProductCategory(1, "categoria"), 2, 3)))
+    );
+  }
 }
