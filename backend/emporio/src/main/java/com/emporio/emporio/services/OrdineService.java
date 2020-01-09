8685d944ee77292a4d32b8eb83c05609cd5ca0fc
Rik <riccardo.coltrinari@studenti.unicam.it>
2020-01-09 18:58:18 +0100

    Ordine service

--- backend/emporio/src/main/java/com/emporio/emporio/services/OrdineService.java
+++ backend/emporio/src/main/java/com/emporio/emporio/services/OrdineService.java
@@ -1,5 +1,14 @@
 package com.emporio.emporio.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
+
+import com.emporio.emporio.model.Ordine;
+import com.emporio.emporio.repository.OrdineRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -7,6 +16,21 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class OrdineService {
+
+    @Autowired
+    private OrdineRepository orderRepository;
+    
+    public List<Ordine> getAllNotAssignedOrders() {
+        List<Ordine> orders = orderRepository.findAll()
+                                             .stream()
+                                             .filter(c -> c.getOrderConsegna()==null)
+                                             .collect(Collectors.toList());
+
+        if(orders.isEmpty())
+            throw new EntityNotFoundException("Non ci sono ordini non in consegna!");
+
+        return orders;
+    }
 
     
 }
