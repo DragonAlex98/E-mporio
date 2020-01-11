395049934d58cf7acbe0b09b9c15fbe5044f26a6
Ugo <ugo.spinucci@studenti.unicam.it>
2020-01-11 17:43:11 +0100

    importo su modulo delivery

--- frontend/emporio/src/app/delivery/delivery.module.ts
+++ frontend/emporio/src/app/delivery/delivery.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { DeliveryRoutingModule } from '@src/app/delivery/delivery-routing.module';
 import { CreateDeliveryComponent } from '@src/app/delivery/create-delivery/create-delivery.component';
 import { MaterialModule } from '../material/material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [CreateDeliveryComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
     DeliveryRoutingModule,
     MaterialModule
   ],
