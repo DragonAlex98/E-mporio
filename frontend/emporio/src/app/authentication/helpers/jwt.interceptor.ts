ac4f25b656ec0b50d07cd64b638b3cb47cacdc87
DragonAlex98 <alessandro.antinori@studenti.unicam.it>
2020-01-16 20:16:42 +0100

    Aggiunta gestione refresh token

--- frontend/emporio/src/app/authentication/helpers/jwt.interceptor.ts
+++ frontend/emporio/src/app/authentication/helpers/jwt.interceptor.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Observable, BehaviorSubject, throwError } from 'rxjs';
 
 import { environment } from '@src/environments/environment';
 import { AuthenticationService } from '@src/app/authentication/services/authentication.service';
+import { catchError, switchMap, filter, take, tap, map } from 'rxjs/operators';
+import { error } from 'util';
 
 @Injectable({
     providedIn: 'root'
@@ -11,21 +13,76 @@ import { AuthenticationService } from '@src/app/authentication/services/authenti
 export class JwtInterceptor implements HttpInterceptor {
     constructor(private authenticationService: AuthenticationService) { }
 
+    addAuthenticationToken(request: HttpRequest<any>, token: string) {
+        if (!token) {
+            return request;
+        }
+
+        return request.clone({
+            setHeaders: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    }
+
+    private refreshTokenInProgress = false; //funge da semaforo
+    private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+
+    private handleUnauthorizedError(request: HttpRequest<any>, next: HttpHandler) {
+        if (!this.refreshTokenInProgress) {
+            // se non è stata ancora fatta richiesta per un nuovo token, la faccio
+            this.refreshTokenInProgress = true;
+            this.refreshTokenSubject.next(null);
+
+            console.log("Vecchio token: " + this.authenticationService.currentUserValue.token);
+
+            return this.authenticationService.refresh().pipe(
+                switchMap(newToken => {
+                    // sia che ho un errore, sia che il refresh token
+                    // non è scaduto, sblocco le richieste pendenti
+                    if(!newToken || newToken.length <= 0) {
+                        this.authenticationService.logout();
+                        this.refreshTokenInProgress = false;
+                        this.refreshTokenSubject.next(newToken);
+                        return throwError(error);
+                    }
+                    this.refreshTokenInProgress = false;
+                    this.refreshTokenSubject.next(newToken);
+                    return next.handle(this.addAuthenticationToken(request, newToken));
+              }));
+        } else {
+            // altrimenti mi metto in attesa
+            return this.refreshTokenSubject.pipe(
+                filter(token => token != null), // attesa
+                take(1), // chiudo lo stream una volta risolta la richiesta
+                switchMap(token => {
+                    // risolvo le richieste pendenti
+                    return next.handle(this.addAuthenticationToken(request, token));
+                }));
+        }
+    }
+
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // add auth header with jwt if user is logged in and request is to api url
         console.log('Intercettato');
         
         const currentUser = this.authenticationService.currentUserValue;
-        const isLoggedIn = currentUser && currentUser.token; // veramente ambiguo
+        const isLoggedIn = currentUser && currentUser.token;
         const isApiUrl = request.url.startsWith(environment.apiUrl);
-        if (isLoggedIn && isApiUrl) {
-            request = request.clone({
-                setHeaders: {
-                    Authorization: `Bearer ${currentUser.token}`
-                }
-            });
+        // devo rimuovere l'authorization header per refreshare il token,
+        // perchè se il token è scaduto, la richiesta viene bloccata lato backend
+        // dal JwtTokenAuthenticationFilter
+        if (isLoggedIn && isApiUrl && !request.url.endsWith('/auth/refresh')) {
+            request = this.addAuthenticationToken(request, currentUser.token);
         }
 
-        return next.handle(request);
+        // effettua la richiesta
+        return next.handle(request).pipe(catchError(error => {
+            if (error instanceof HttpErrorResponse && error.status === 401) {
+                // se ricevo errore 401 UNAUTHORIZED, lo gestisco
+                return this.handleUnauthorizedError(request, next);
+            } else {
+                return throwError(error);
+            }
+        }));
     }
 }
--- frontend/emporio/src/app/authentication/services/authentication.service.ts
+++ frontend/emporio/src/app/authentication/services/authentication.service.ts
@@ -1,46 +1,80 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { map, tap, switchMap } from 'rxjs/operators';
 
 import { environment } from '@src/environments/environment';
 import { User } from '@src/app/authentication/models/user';
+import { stringify } from 'querystring';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthenticationService {
 
-  private currentUserSubject: BehaviorSubject<User>;
-  public currentUser: Observable<User>;
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
 
-  constructor(private http: HttpClient) {
-      this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
-      this.currentUser = this.currentUserSubject.asObservable();
-  }
+    constructor(private http: HttpClient, private router: Router) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
 
-  public get currentUserValue(): User {
-      return this.currentUserSubject.value;
-  }
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
 
-  login(username: string, password: string) {
-      return this.http.post<any>(`${environment.apiUrl}/auth/signin`, { username, password })
-          .pipe(map(user => {
-              // login successful if there's a jwt token in the response
-              if (user && user.token) {
-                  // store user details and jwt token in local storage to keep user logged in between page refreshes
-                  localStorage.setItem('currentUser', JSON.stringify(user));
-                  this.currentUserSubject.next(user);
-              }
+    login(username: string, password: string) {
+        return this.http.post<any>(`${environment.apiUrl}/auth/signin`, { username, password })
+            .pipe(map(user => {
+                // login successful if there's a jwt token in the response
+                if (user && user.token) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                    this.currentUserSubject.next(user);
+                }
 
-              return user;
-          }));
-  }
+                return user;
+            }));
+    }
 
-  logout() {
-      // remove user from local storage to log user out
-      localStorage.removeItem('currentUser');
-      this.currentUserSubject.next(null);
-  }
+    refresh() {
+        var refresh = this.getRefreshToken();
+        return this.http.post(`${environment.apiUrl}/auth/refresh`, { refresh }, { responseType: 'text' }).pipe(
+            tap(newToken => {
+                this.updateToken(newToken);
+            }));
+    }
 
+    logout() {
+        // remove user from local storage to log user out
+        this.currentUserSubject.next(null);
+        this.currentUser = null;
+        this.removeToken();
+        // ritorno alla home
+        this.router.navigate(['/']);
+    }
+
+    getJwtToken() {
+        var token = JSON.parse(localStorage.getItem('currentUser'))['token'];
+        return (!token) ? null : token;
+    }
+
+    private getRefreshToken() {
+        var refresh = JSON.parse(localStorage.getItem('currentUser'))['refresh'];
+        return (!refresh) ? null : refresh;
+    }
+
+    private updateToken(token) {
+        var old = JSON.parse(localStorage.getItem('currentUser'));
+        old['token'] = token;
+        var newUser = JSON.stringify(old);
+        localStorage.setItem('currentUser', newUser);
+        this.currentUserValue.token = token;
+    }
+
+    private removeToken() {
+        localStorage.removeItem('currentUser');
+    }
 }
