7b726b1986638f52da7d8df09f8fd7ec4393ef9d
Ugo <ugo.spinucci@studenti.unicam.it>
2020-01-31 17:39:02 +0100

    Fatto semplice test su service consegna

--- backend/emporio/src/test/java/com/emporio/emporio/integration/service/ConsegnaServiceIntegrationTest.java
+++ backend/emporio/src/test/java/com/emporio/emporio/integration/service/ConsegnaServiceIntegrationTest.java
@@ -1,9 +1,19 @@
 package com.emporio.emporio.integration.service;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.emporio.emporio.model.Consegna;
+import com.emporio.emporio.model.Fattorino;
 import com.emporio.emporio.repository.ConsegnaRepository;
 import com.emporio.emporio.services.ConsegnaService;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -33,6 +43,29 @@ public class ConsegnaServiceIntegrationTest {
 
     @MockBean
     private ConsegnaRepository consegnaRepository;
+
+    @Before
+    public void configure() {
+
+        Long idFatt = new Long(2);
+        Consegna delivery = Consegna.builder().idConsegna(1).fattorino(Fattorino.builder().id(idFatt).build()).build();
+        List<Consegna> list = new ArrayList<Consegna>();
+        list.add(delivery);
+
+        Mockito.when(consegnaRepository.findByfattorino_id(idFatt)).thenReturn(list);
+
+    }
+
+    @Test
+    public void whenGetDeliveryByFattorino_ThenReturnList() {
+
+        Fattorino fattorino = Fattorino.builder().id(new Long(2)).build();
+
+        List<Consegna> found = consegnaService.getDeliveryByFattorino(fattorino);
+
+        assertTrue(found.size() != 0);
+
+    }
 
 
 }
